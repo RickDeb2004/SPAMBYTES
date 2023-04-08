1227bfa78955626731dfc7311a451cb128dfec3a
Meru45 <87946072+Meru45@users.noreply.github.com>
2023-04-08 10:51:20 +0530

    Contract Connected to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -6,84 +6,53 @@ import NavBar from "./components/NavBar";
 import ButtonPage from "./pages/ButtonPage";
 import ModalPage from "./pages/ModalPage";
 import Button from "./components/Button";
-import Contract from "./artifacts/contracts/DeEcomm.sol/DeEcomm.json";
-
-const { ethereum } = window;
+import abi from "./artifacts/contracts/DeEcomm.sol/DeEcomm.json";
 
 const App = () => {
-    const [errorMessage, setErrorMessage] = useState(null);
     const [defaultAccount, setDefaultAccount] = useState(null);
-    const [userBalance, setUserBalance] = useState(null);
-    const [connButtonText, setConnButtonText] = useState("Connect Wallet");
     const [provider, setProvider] = useState(null);
     const [contract, setContract] = useState(null);
     const [signer, setSigner] = useState(null);
 
-    const connectWalletHandler = () => {
-        if (window.ethereum && window.ethereum.isMetaMask) {
-            console.log("MetaMask Here!");
+    const connectWallet = async () => {
+        const contractAddress = "0x4ba2C480d0e25c6fE7fad819Eaa8CEaa7cB6c82B";
+        const contractABI = abi.abi;
+        try {
+            const { ethereum } = window;
 
-            window.ethereum
-                .request({ method: "eth_requestAccounts" })
-                .then((result) => {
-                    accountChangedHandler(result[0]);
-                    setConnButtonText("Connected...");
-                    getUserBalance(result[0]);
-                })
-                .catch((error) => {
-                    setErrorMessage(error.message);
+            if (ethereum) {
+                const account = await ethereum.request({
+                    method: "eth_requestAccounts",
                 });
-        } else {
-            console.log("Need to install MetaMask");
-            setErrorMessage(
-                "Please install MetaMask browser extension to interact"
-            );
+
+                window.ethereum.on("chainChanged", () => {
+                    window.location.reload();
+                });
+
+                window.ethereum.on("accountsChanged", () => {
+                    window.location.reload();
+                });
+
+                const provider = new ethers.providers.Web3Provider(ethereum);
+                const signer = provider.getSigner();
+                const contract = new ethers.Contract(
+                    contractAddress,
+                    contractABI,
+                    signer
+                );
+                setDefaultAccount(account.toString());
+                setProvider(provider);
+                setSigner(signer);
+                setContract(contract);
+            } else {
+                alert("Please install metamask");
+            }
+        } catch (error) {
+            console.log(error);
         }
     };
 
-    const accountChangedHandler = (newAccount) => {
-        setDefaultAccount(newAccount);
-        getUserBalance(newAccount);
-        updateEthers();
-    };
-
-    const chainChangedHandler = () => {
-        window.location.reload();
-    };
-    window.ethereum.on("accountsChanged", accountChangedHandler);
-
-    window.ethereum.on("chainChanged", chainChangedHandler);
-
-    const updateEthers = () => {
-        let tempProvider = new ethers.providers.Web3Provider(ethereum);
-        setProvider(tempProvider);
-
-        let tempSigner = tempProvider.getSigner();
-        setSigner(tempSigner);
-
-        const contractAddress = "0xA1d1b966d4E3Be90389846e4ec3Da1Fb3243Ba9b"; //test network address 0xa9FcB826138c55E2870C251C2EA56E04ac1Cb6F2
-        let tempContract = new ethers.Contract(
-            contractAddress,
-            Contract.abi,
-            tempSigner
-        );
-        setContract(tempContract);
-    };
-
-    const getUserBalance = (account) => {
-        window.ethereum
-            .request({ method: "eth_getBalance", params: [account, "latest"] })
-            .then((balance) => {
-                setUserBalance(ethers.utils.formatEther(balance));
-            })
-            .catch((error) => {
-                setErrorMessage(error.message);
-            });
-    };
-
-    const getContract = () => {
-        console.log(contract);
-    };
+    console.log(contract);
 
     return (
         <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
@@ -98,12 +67,11 @@ const App = () => {
                     primary
                     type="button"
                     className="nav__connect mb-5"
-                    onClick={connectWalletHandler}
+                    onClick={connectWallet}
                 >
                     Connect
                 </Button>
             )}
-            <div>Balance: {userBalance}</div>
 
             <NavBar />
             <div className="col-span-5">
