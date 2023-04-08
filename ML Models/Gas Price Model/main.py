44131f1937665b4d892f35fc7834b84272c4c995
GreatRSingh <rakshitsingh421@gmail.com>
2023-04-08 06:01:36 +0530

    api

--- ML Models/Gas Price Model/main.py
+++ ML Models/Gas Price Model/main.py
@@ -1,5 +1,8 @@
 import sklearn
 import flask
+import pickle
+import os
+import numpy as np
 
 app = flask.Flask(__name__)
 
@@ -7,6 +10,13 @@ app = flask.Flask(__name__)
 def index():
     return 'Hello World All men!'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+with open(os.path.dirname(__file__) + "/Gas Price Modelmodel.pkl" , 'rb') as f:
+    model = pickle.load(f)
 
+@app.route('/<int:id>/<int:pid>/pi')
+def pdf_template(id, pid):
+    prediction = model.predict(np.array([[id, pid]]))
+    return str(prediction)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0' , debug=True)
