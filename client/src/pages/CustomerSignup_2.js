14762559a9fc145da3da78f7c4176241b2895494
Kinjal Bhattacharyya <bkinjal2004@gmail.com>
2023-04-09 08:05:35 +0530

    Update CustomerSignup_2.js

--- client/src/pages/CustomerSignup_2.js
+++ client/src/pages/CustomerSignup_2.js
@@ -11,6 +11,7 @@ const CustomerSignup_2 = () => {
     const [teritory, setTeritory] = useState("");
     const [city, setCity] = useState("");
     const [pincode, setPincode] = useState("");
+    const [state,setState]= useState("");
     const { navigate } = useNavigation();
 
     const options = useMemo(() => countryList().getData(), []);
@@ -29,39 +30,39 @@ const CustomerSignup_2 = () => {
     };
 
     return (
-        <div>
+        <div className=" bg-[url('../public/960x0-1@2x.png')] bg-cover h-screen">
             <h1>Buyer SignUp</h1>
             <form onSubmit={handleSubmit}>
-                <input
+                <input className="absolute top-[200px] left-[600px]"
                     value={address}
                     placeholder="Enter Full address"
                     onChange={(e) => setAddress(e.target.value)}
                     type="address"
                 />
-                <Select
+                <Select className="absolute top-[400px] left-[480px] w-[500px]"
                     options={options}
                     value={Country}
                     onChange={changeHandler}
                 />
-                <input
-                    value={teritory}
-                    placeholder="Enter State/Teritory"
-                    onChange={(e) => setTeritory(e.target.value)}
+                <input className="absolute top-[300px] left-[450px]"
+                    value={state}
+                    placeholder="Enter State/Territory"
+                    onChange={(e) => setState(e.target.value)}
                     type="text"
                 />
-                <input
+                <input className="absolute top-[300px] left-[650px]"
                     value={city}
                     placeholder="Enter City"
                     onChange={(e) => setCity(e.target.value)}
                     type="text"
                 />
-                <input
+                <input className="absolute top-[300px] left-[850px]"
                     value={pincode}
                     placeholder="Enter city pincode"
                     onChange={(e) => setPincode(e.target.value)}
                     type="number"
                 />
-                <Button primary type="submit">
+                <Button primary type="submit" className="absolute top-[550px] left-[680px]">
                     Continue
                 </Button>
             </form>
